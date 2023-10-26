b9530dad45f2f15b75fc22fc43c52f83de912c21
Henrique Ramos <henrique.mrcr@gmail.com>
2023-10-26 04:29:18 +0100

    teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MachinesModule } from './machines/machines.module';
 
 @Module({
-  imports: [],
+  imports: [MachinesModule],
   controllers: [AppController],
   providers: [AppService],
 })
